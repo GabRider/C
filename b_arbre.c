c41f9cb90bacddb5670bb114242918d16699f82d
Gabriel <gabrielstrano98@gmail.com>
2017-06-07 16:27:14 +0200

    b_arbre split basic odne

--- b_arbre.c
+++ b_arbre.c
@@ -1,11 +1,18 @@
 #include "b_arbre.h"
 #include <stdlib.h>
 #include <stdio.h>
+Element*create_Element(int clef,Page*pg)
+{
+  Element* tmp = (Element*)malloc(sizeof(Element));
+  tmp->clef=clef;
+  tmp->pg=NULL;
+  return tmp;
+}
 Page* new_page(int order)
 {
   Page*pg=(Page*)malloc(sizeof(Page));
   Element*tab=(Element*)malloc((order*2+2)*sizeof(Element));
-  if (tab||pg) {
+  if (!tab||!pg) {
     exit(0);
   }
   pg->order=order;
@@ -16,9 +23,7 @@ Page* new_page(int order)
 Page*insert(Page* b_tree,int clef)
 {
   int depth=0;
-  Element* tmp = (Element*)malloc(sizeof(Element));
-  tmp->clef=clef;
-  tmp->pg=NULL;
+  Element* tmp = create_Element(clef,NULL);
   return insert_case(b_tree,tmp,depth);
 }
 Page*insert_case(Page* b_tree,Element*cell,int depth)
@@ -26,6 +31,31 @@ Page*insert_case(Page* b_tree,Element*cell,int depth)
   Page* current_page= b_tree;
   place(current_page,cell);
 return b_tree;
+}
+void split(Page* pg, Element*cell)
+{
+  int page_order= pg->order*2+2;
+  Page* right= new_page(pg->order);
+  Page* left= new_page(pg->order);
+  //mettre les petits nombres dans une nouvelle page
+  for (int i = page_order-pg->order; i < page_order; i++) {
+    Element* tmp = create_Element(pg->tab[i].clef,pg->tab[i].pg);
+    place(right, tmp);
+    pg->nb-=1;
+  }
+  //mettre les petits nombres dans une nouvelle page
+  for (int i =1; i <= pg->nb; i++) {
+    Element* tmp = create_Element(pg->tab[i].clef,pg->tab[i].pg);
+    place(left, tmp);
+    pg->nb-=1;
+  }
+  //déplace le nombre du milieu au début de la page
+  pg->tab[1]= pg->tab[page_order-pg->order-1];
+  pg->tab[0].pg=left;
+  pg->tab[1].pg=right;
+  cell->clef=pg->tab[1].clef;
+  cell->pg= right;
+
 }
 int position(Page*pg,int clef)
 {
@@ -38,18 +68,18 @@ int position(Page*pg,int clef)
       return i;
     }
   }
-  return pg->nb;
+  return pg->nb+1;
 }
 int place(Page* pg,Element* cell)
 {
-  int new_place=position(pg,cell.clef);
+  int new_place=position(pg,cell->clef);
   if(new_place<0)
   {
     return 0;
   }
   pg->nb+=1;
-  for (int i = pg->nb; i >0; i--) {
-    if (i!=1) {
+  for (int i = pg->nb; i >=new_place; i--) {
+    if (i!=new_place) {
       pg->tab[i]=pg->tab[i-1];
     }
     else
@@ -57,5 +87,9 @@ int place(Page* pg,Element* cell)
       pg->tab[i]=*cell;
     }
   }
-    return 1; 
+  if (pg->nb==pg->order*2+1)
+  {
+split(pg,cell);
+  }
+    return 1;
 }
--- test_b_arbre.c
+++ test_b_arbre.c
@@ -1,5 +1,10 @@
 #include "b_arbre.h"
 int main(int argc, char const *argv[]) {
-  /* code */
+  Page*tree= new_page(2);
+  insert(tree,3);
+  insert(tree,10);
+  insert(tree,20);
+  insert(tree,5);
+  insert(tree,6);
   return 0;
 }
